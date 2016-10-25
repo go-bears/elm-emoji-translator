e3bd8022965510fe577d5cdbb5acb32dd0219a37
go-bears <melissa.fabros@gmail.com>
2016-10-25 13:44:07 -0700

    add model for elmoji cypher

---
+++ Model.elm
@@ -0,0 +1,25 @@
+module Model exposing (..)
+
+
+type Direction
+    = TextToEmoji
+    | EmojiToText
+
+type alias Model =
+    { currentText : String
+    , direction : Direction
+    , selectedKey : String
+    }
+
+
+
+init : Model
+init =
+    { currentText = ""
+    , direction = TextToEmoji
+    , selectedKey = defaultKey
+    }
+
+defaultKey : String 
+defaultKey =
+    "😅"
